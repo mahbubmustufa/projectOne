8e046b7a14f42fbc3bc60c9ee4f52a4d25be0900
mahbub <mahbubmustufa@gmail.com>
2023-08-27 06:43:25 +0600

    formik installation ok

--- one/src/COMPONENTS/Form.jsx
+++ one/src/COMPONENTS/Form.jsx
@@ -1,24 +1,51 @@
 import React, { useState } from "react";
+import { Formik, useFormik } from "formik";
 
 const Form = () => {
-	const [name, setName] = useState("");
-	const handleNameChange = (e) => {
-		e.preventDefault();
-		setName(e.target.value);
-		console.log(name);
-	};
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+		onSubmit: (values, { resetForm }) => {
+			console.log(values);
+			resetForm({ values: "" });
+		},
+	});
 	return (
-		<form>
+		<form onSubmit={formik.handleSubmit}>
 			<div>
 				<label htmlFor="name">Name:</label>
 				<input
 					name="name"
 					id="name"
 					type="text"
-					value={name}
-					onChange={handleNameChange}
+					value={formik.values.name}
+					onChange={formik.handleChange}
 				/>
 			</div>
+			<div>
+				<label htmlFor="email">Email:</label>
+				<input
+					name="email"
+					id="email"
+					type="email"
+					value={formik.values.email}
+					onChange={formik.handleChange}
+				/>
+			</div>
+			<div>
+				<label htmlFor="password">Password:</label>
+				<input
+					name="password"
+					id="password"
+					type="password"
+					value={formik.values.password}
+					onChange={formik.handleChange}
+				/>
+			</div>
+			<button type="submit">Signup</button>
 		</form>
 	);
 };
